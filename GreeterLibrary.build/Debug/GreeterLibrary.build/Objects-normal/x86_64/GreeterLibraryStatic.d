437a5c6e4f40841de7b775a70ee7cba6dc53ad15
a1ex.solovyov <a1ex.solovyov10@gmail.com>
2023-12-13 14:29:53 +0300

    11.01_First commit

---
+++ GreeterLibrary.build/Debug/GreeterLibrary.build/Objects-normal/x86_64/GreeterLibraryStatic.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/a/Desktop/Programming/Netology/cppm-homeworks/cppm-homework_11/cppm-homework-11.01/GreeterLibrary/GreeterLibraryStatic.cpp \
+  /Users/a/Desktop/Programming/Netology/cppm-homeworks/cppm-homework_11/cppm-homework-11.01/GreeterLibrary/GreeterLibraryStatic.hpp
